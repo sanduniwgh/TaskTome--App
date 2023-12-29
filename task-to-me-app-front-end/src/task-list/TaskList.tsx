825ebfb5575c68781198d2fee373ee77b4ada865
Sanduni Haththotuwa <sandunihaththotuwa@gmail.com>
2023-12-29 18:39:13 +0530

    complete TaskList.tsx

--- task-to-me-app-front-end/src/task-list/TaskList.tsx
+++ task-to-me-app-front-end/src/task-list/TaskList.tsx
@@ -1,5 +1,29 @@
+import {getAllTasks} from "../services/taskservices.tsx";
+import {useInsertionEffect} from "react";
+import {useTaskDispatcher, useTaskList} from "../context/TaskContext.tsx";
+import {useUser} from "../context/UserContext.tsx";
+import {Task} from "../task/Task.tsx";
 export function TaskList() {
+
+    const taskList = useTaskList();
+    const taskDispatcher = useTaskDispatcher();
+    const user = useUser();
+
+    useInsertionEffect(() => {
+        getAllTasks(user!.email!).then(taskList => {
+            taskDispatcher({type: 'set-list', taskList});
+        }).catch(err => {
+            alert("Failed to load tasks");
+        })
+        return ()=>{
+            taskDispatcher({type: 'set-list', taskList: []});
+        }
+    }, []);
     return (
-        <></>
+        <>
+            {taskList.map(task =>
+            <Task key={task.id}{...task} />
+            )}
+        </>
     );
 }
